19d856bdbba0f3e0fdd9c8e02a1bceb4816915a8
纽特 <coorchice.cb@alibaba-inc.com>
2019-09-17 11:59:08 +0800

    update

--- js/index.js
+++ js/index.js
@@ -2,9 +2,17 @@ function input() {
     const input = document.getElementById('input').value;
     try {
         const decode = unGzip(input);
+        console.log("decode = " + decode);
         let output = JSON.stringify(JSON.parse(decode), null, 4);
         $("#output").text(output);
     } catch (error) {
+        try {
+            const decode = unGzip(decodeURI(input));
+            console.log("decode = " + decode);
+            let output = JSON.stringify(JSON.parse(decode), null, 4);
+            $("#output").text(output);
+        } catch (error) {
 
+        }
     }
 }
